77ef5580626b9bc8b1b8363c4ec61c5400967d6f
Adzooda <63106485+Adzooda@users.noreply.github.com>
2020-04-03 14:07:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+let name = "Adam"
+let age = 14;
+let school = " Calthorpe Park school"
+console.log ("hi, my name is " + name);
+console.log ("I am " + age + " years old");
+console.log ("I attend" + school);
+
+
+let NumberOne;
+let NumberTwo;
+let total;
+
+const addition=(NumberOne, NumberTwo) => {
+   total = NumberOne + NumberTwo;
+    console.log(total);
+}
+
+addition(20, 30);
+addition(30, 70)
+addition(40, 15)
